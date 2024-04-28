33cf5a62b9498983fe20b77dd814cd8cb31b6284
caodangtinh <caodangtinh04022003@gmail.com>
2024-04-29 00:08:10 +0700

    update: route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import About from "@/pages/About";
 import Dashboard from "@/pages/Dashboard";
 import Home from "@/pages/Home";
+import SignIn from "@/pages/SignIn";
+import SignUp from "@/pages/SignUp";
+
+import Projects from "./pages/Projects";
 
 const router = createBrowserRouter([
   {
@@ -9,8 +14,24 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignIn />,
+  },
+  {
     path: "/dashboard",
     element: <Dashboard />,
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
   },
 ]);
 

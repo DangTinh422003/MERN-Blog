f28bc14a35415efbebaff6db1b2da5358dd729a8
caodangtinh <caodangtinh04022003@gmail.com>
2024-04-29 00:06:12 +0700

    update: import alias

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Dashboard from "@/pages/Dashboard";
+import Home from "@/pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <RouterProvider router={router} />;
 }

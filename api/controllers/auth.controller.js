6c5b14de018e9dc94c176a0e1daf68d107abbfe6
caodangtinh <caodangtinh04022003@gmail.com>
2024-05-03 23:25:37 +0700

    feat: signup functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,19 +7,16 @@ class Auth {
   async signup(req, res, next) {
     try {
       const { username, email, password } = req.body;
-
       if (!username || !email || !password) {
         return next(errorHandler(400, "All fields are required"));
       }
-
       const hashedPassword = bcryptjs.hashSync(password, 10);
-
       await userModel.create({
         username,
         email,
         password: hashedPassword,
       });
-      res.send("sign-up");
+      res.json("sign-up successful");
     } catch (error) {
       next(error);
     }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,47 @@
-import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Alert, Button, Label, TextInput } from "flowbite-react";
+import { CgSpinnerTwo } from "react-icons/cg";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      setErrorMessage("All fields are required");
+      return;
+    }
+    try {
+      setIsLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!data.success) {
+        return setErrorMessage(data.message);
+      }
+
+      if (res.ok) {
+        navigate("/sign-in");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -27,25 +67,45 @@ const SignUp = () => {
 
         {/* right */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                onChange={handleChange}
+                type="text"
+                placeholder="Username"
+                id="username"
+              />
             </div>
             <div>
               <Label value="Your email" />
               <TextInput
-                type="text"
+                onChange={handleChange}
+                type="email"
                 placeholder="name@company.com"
                 id="email"
               />
             </div>
             <div>
               <Label value="Your password" />
-              <TextInput type="password" placeholder="Password" id="password" />
+              <TextInput
+                onChange={handleChange}
+                type="password"
+                placeholder="Password"
+                id="password"
+              />
             </div>
-            <Button gradientDuoTone={"purpleToPink"} type="submit">
-              sign up
+            <Button
+              disabled={isLoading}
+              gradientDuoTone={"purpleToPink"}
+              type="submit"
+              className="flex items-center justify-center"
+            >
+              {isLoading ? (
+                <CgSpinnerTwo className="animate-spin text-3xl" />
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
 
@@ -55,6 +115,12 @@ const SignUp = () => {
               Sign In
             </Link>
           </div>
+
+          {errorMessage && (
+            <Alert className="mt-5" color={"failure"}>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>

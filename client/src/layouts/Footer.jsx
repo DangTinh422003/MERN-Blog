b1bbb244f5197dfb0642e4affc6da70e9056af99
caodangtinh <caodangtinh04022003@gmail.com>
2024-05-04 00:19:54 +0700

    feat: add footer

--- client/src/layouts/Footer.jsx
+++ client/src/layouts/Footer.jsx
@@ -1,5 +1,25 @@
+import { Footer as FooterContainer, Button } from "flowbite-react";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterContainer container className="border border-t-8 border-teal-500">
+      <div className="">
+        <Link
+          to={"/"}
+          className="flex items-center gap-x-2 self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <Button
+            gradientDuoTone={"purpleToBlue"}
+            className="text-white rounded-lg inline-block pointer-events-none"
+          >
+            Dang&apos;s
+          </Button>
+          Blog
+        </Link>
+      </div>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,6 +1,7 @@
-import { Footer } from "flowbite-react";
-import Header from "./Header";
 import { Outlet } from "react-router-dom";
+
+import Footer from "./Footer";
+import Header from "./Header";
 
 const MainLayout = () => {
   return (
